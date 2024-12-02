d8c4bfdbd4615fbbd7ba8787db1db1bf3aead8c9
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-12-02 16:46:35 -0500

    drawing square using for loop

--- turtle-dot-painting/main.py
+++ turtle-dot-painting/main.py
@@ -9,16 +9,9 @@ timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
 # Draw a square
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-
-
-
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 
 
