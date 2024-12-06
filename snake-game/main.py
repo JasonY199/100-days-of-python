377e9655c7a0c630346f829200c17979ef74cf28
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-12-06 18:39:51 -0500

    snake can now move

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Jason's Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
@@ -16,8 +18,18 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
-
-
+is_playing = True
+while is_playing:
+    screen.update()
+    time.sleep(0.1)
+    # Move the snake
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    # Move the first segment
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 
