86554ff4e04e414c3113ac45ac6ad265fcc20faa
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-12-06 19:52:53 -0500

    user can now control the snake using arrow keys

--- snake-game/main.py
+++ snake-game/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from snake import Snake
 import time
 
+
 # Set up the screen
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,15 +13,19 @@ screen.tracer(0)
 # Create the snake
 snake = Snake()
 
+# Listen for key presses
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 # Game loop
 is_playing = True
 while is_playing:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
-
-
 
 # Keep the window open
 screen.exitonclick()
--- snake-game/snake.py
+++ snake-game/snake.py
@@ -1,7 +1,13 @@
 from turtle import Turtle
 
+
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
     def __init__(self):
@@ -9,11 +15,13 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
-    def create_snake(self):
+    def create_snake(self)
+        '''Create the snake'''
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
     def add_segment(self, position):
+        '''Add a new segment to the snake'''
         new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
@@ -21,8 +29,29 @@ class Snake:
         self.segments.append(new_segment)
 
     def move(self):
+        '''Move the snake forward'''
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+    
+    def up(self):
+        '''Move the snake up'''
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self):
+        '''Move the snake down'''
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    
+    def left(self):
+        '''Move the snake left'''
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        '''Move the snake right'''
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
